c91d0e04be28e0a314c1a0229a5ba3b76c581bb2
Alencar31 <alencarw@hotmail.com>
2020-02-25 10:40:17 -0300

    Update seller and Custon service exception

--- SalesWebMvc2017/Controllers/SellersController.cs
+++ SalesWebMvc2017/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc2017.Models;
 using SalesWebMvc2017.Models.ViewModels;
 using SalesWebMvc2017.Services;
+using SalesWebMvc2017.Services.Exceptions;
 
 namespace SalesWebMvc2017.Controllers
 {
@@ -77,6 +78,46 @@ namespace SalesWebMvc2017.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
