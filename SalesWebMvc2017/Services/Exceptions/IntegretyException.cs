15f87f5ff2b9fee68e4e1cc6d74327a11a0302f0
Alencar31 <alencarw@hotmail.com>
2020-03-28 18:13:18 -0300

    Exception handing for delete (referential integrity)

---
+++ SalesWebMvc2017/Services/Exceptions/IntegretyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMvc2017.Services.Exceptions
+{
+    public class IntegretyException : ApplicationException
+    {
+        public IntegretyException(string message) : base(message)
+        {
+        }
+    }
+}
